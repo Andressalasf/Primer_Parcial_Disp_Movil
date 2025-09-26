0c910f46d8f01c479ba7f618fdc99bd982997408
Andressalasf <humkyjimp@gmail.com>
2025-09-26 16:25:04 -0500

    Modificando la pagina home para hacerla mas bonita

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -37,19 +37,33 @@ class HomeScreen extends StatelessWidget {
           ),
       ),
       body: Center(
-        child: 
-        Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-            Text("Andres Felipe Salas - 192164", style: TextStyle(fontSize: 20, color: Colors.indigo),),
-            SizedBox(height: 15,),
-            Text("Brian Matheo Alvarez -192219", style: TextStyle(fontSize: 20, color: Colors.indigo),),
-            SizedBox(height: 15,),
-            Text("Programacion de dispositivos moviles", style: TextStyle(fontSize: 20, color: Colors.indigo),)
-
-
-      ])),
+            Card(
+              color: Colors.indigo,
+              elevation: 5,
+              
+              shadowColor: Colors.blueAccent,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column( 
+                  children: [SizedBox(height: 10,),
+                    Text("Andres Felipe Salas - 192164", style: TextStyle(fontSize: 20, color: Colors.white)),
+                    SizedBox(height: 10,),
+                    Text("Brian Matheo Alvarez -192219", style: TextStyle(fontSize: 20, color: Colors.white)),
+                    SizedBox(height: 10,),
+                    Text("Programacion de dispositivos moviles", style: TextStyle(fontSize: 20, color: Colors.white)),
+                    SizedBox(height: 10,),
+                  ],
+                ),
+              ),
+            ),
+           
+          ],
+        ),
+      ),
     );
   }
 }
